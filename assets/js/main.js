c772d0841c1de9dbda0fdd7fec1c531ff6213c91
Quaselz <paul.geppert94@freenet.de>
2023-05-05 11:19:16 +0200

    adding functions to proejct missing styling

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+//getting elements
+const myForm = document.querySelector("#form");
+//output elements
+const outputBasalRateKcalDisplay = document.querySelector(
+	"#outputBasalRateKcal"
+);
+const outputBasalRateKJDisplay = document.querySelector("#outputBasalRateKJ");
+const ooutputTotalRateKcalDisplay = document.querySelector(
+	"#outputTotalRateKcal"
+);
+const outputTotalRateKJDisplay = document.querySelector("#outputTotalRateKJ");
+
+// setting up form
+myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	//getting values
+	const age = Number(document.querySelector("#age").value);
+	const gender = document.querySelector('input[name="gender"]:checked').value;
+	const activity = document.querySelector("#activity").value;
+	let height = document.querySelector("#height").value;
+	const heightMassUnit = document.querySelector("#heightUnit").value;
+
+	let weight = document.querySelector("#weight").value;
+	const weightMassUnit = document.querySelector("#weightUnit").value;
+
+	let basalRate, totalRate, totalRateCalc;
+
+	//check height (m or cm)
+	if (heightMassUnit === "m") {
+		height = Number(height) * 100;
+	} else {
+		height = Number(height);
+	}
+
+	// check weight (kg or g)
+	if (weightMassUnit === "g") {
+		weight = Number(weight) / 1000;
+	} else {
+		weight = Number(weight);
+	}
+	//check gender
+	if (gender === "male") {
+		basalRate = 66.47 + 13.7 * weight + 5 * height - 6.8 * age;
+	} else if (gender === "female") {
+		basalRate = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age;
+	} else {
+		alert("Bitte Geschlecht auswählen.");
+		return;
+	}
+	//set PAL-Faktor
+	switch (activity) {
+		case "sleep":
+			totalRateCalc = 0.95;
+			break;
+		case "lay":
+			totalRateCalc = 1.2;
+			break;
+		case "sit":
+			totalRateCalc = 1.5;
+			break;
+		case "sitPlus":
+			totalRateCalc = 1.7;
+			break;
+		case "stayMost":
+			totalRateCalc = 1.9;
+			break;
+		case "stay":
+			totalRateCalc = 2.2;
+			break;
+		default:
+			alert("Try again");
+			break;
+	}
+	//calc totalRate
+	totalRate = basalRate * totalRateCalc;
+
+	//outputs BasalRate
+	outputBasalRateKcalDisplay.innerHTML = basalRate.toFixed(3);
+	outputBasalRateKJDisplay.innerHTML = (basalRate * 4.2).toFixed(3);
+
+	//output totalRate
+	ooutputTotalRateKcalDisplay.innerHTML = totalRate.toFixed(3);
+	outputTotalRateKJDisplay.innerHTML = (totalRate * 4.2).toFixed(3);
+});
